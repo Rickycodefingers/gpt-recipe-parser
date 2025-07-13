2fd2aebe56a3114b495cf2b06c687ed1d19fd6f5
Ricali <felixebejer@gmail.com>
2025-07-13 15:51:52 +1000

    Fix app.py and wsgi.py to properly expose Flask app for deployment

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from invoice_scanner_api import app
 import os
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000))) 
+# This is the Flask app that will be used by gunicorn
+# The app is imported from invoice_scanner_api.py
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False) 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from invoice_scanner_api import app
 
-if __name__ == "__main__":
+# This is the Flask app that will be used by WSGI servers
+# The app is imported from invoice_scanner_api.py
+
+if __name__ == '__main__':
     app.run() 
